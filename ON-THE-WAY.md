b2a93baf5fec55fb72059be25f5ae1d62775a8ab
Klay Wu <793336993@qq.com>
2018-06-02 00:22:15 +0800

    2018-06-01
    details in on-the-way.md

---
+++ ON-THE-WAY.md
@@ -0,0 +1,14 @@
+
+🐶 Name: My-Silly-Music-Player
+
+🐶 Description: A distempered music player~
+
+
+=== 06/01 ===
+
+1. 导航条的布局及相关交互操作;
+2. 完成账号信息的弹框（trigger：focus);
+
+🌄 [导航条](http://123.207.53.192/img/%E5%AF%BC%E8%88%AA%E6%9D%A1.png)
+
+🌄 [账号信息弹框](http://123.207.53.192/img/%E8%B4%A6%E5%8F%B7%E4%BF%A1%E6%81%AF%E5%BC%B9%E7%AA%97.png)
---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $all = $('#playlist,#album,#artist');
+    $all_classify = $('.classify-playlist,.classify-album,.classify-artist');
+    $all.click(function(){
+        var text = $(this).attr('id');
+        console.log(text);
+        $all.removeClass('active');
+        $(this).addClass('active');
+        console.log('.classify-' + text);
+        $all_classify.css('display','none');
+        $('.classify-'+text).css('display','flex');
+    })
+
+    $('[data-toggle="popover"').each(function(){
+        var element = $(this);
+        element.popover({
+            trigger:"focus",
+            placement:"bottom",
+            title:"",
+            html:"true",
+            content:ContentMethod(),
+        })
+    })
+    // .on("mouseenter",function(){
+    //     var _this = this;
+    //     $(this).popover("show");
+    // })
+    // .on("mouseleave",function(){
+    //     var _this = this;
+    //     setTimeout(function(){
+    //         // if(!$(".popover:hover").length){
+    //             $(_this).popover("hide")
+    //         // };
+    //     },100);
+    // });
+});
